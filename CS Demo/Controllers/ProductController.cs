df05389ec79a44897c709ce31b9441383037a267
Vasiliy Fomichev <vasya.fomichev@gmail.com>
2016-02-08 14:36:59 -0800

    Initial solution structure.

---
+++ CS Demo/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Mvc.Controllers;
+
+namespace CSDemo.Controllers
+{
+    public class ProductController : SitecoreController
+    {
+    }
+}
---
+++ CS Demo/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CSDemo.Contracts;
+
+namespace CSDemo.Models
+{
+    public class Product : IProduct
+    {
+    }
+}
