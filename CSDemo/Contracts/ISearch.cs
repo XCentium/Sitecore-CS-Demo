b5c297798d94cfeeb548e19a6ff0a6476506e68b
Vasiliy Fomichev <vasya.fomichev@gmail.com>
2016-02-23 13:15:17 -0800

    Added search stubs.

---
+++ CSDemo/Contracts/ISearch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSDemo.Models.Search;
+
+namespace CSDemo.Contracts
+{
+    public interface ISearch
+    {
+        string Query { get; set; }
+        IList<ISearchResult> Results { get; set; }
+        int PageSize { get; set; }
+    }
+}
---
+++ CSDemo/Controllers/SearchController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CSDemo.Models.Search;
+using Glass.Mapper.Sc;
+using Sitecore.Links;
+using Sitecore.Mvc.Presentation;
+
+namespace CSDemo.Controllers
+{
+    public class SearchController : Controller
+    {
+        #region Fields
+
+        private readonly ISitecoreService _service;
+
+        #endregion
+
+        #region Constructors
+
+        public SearchController(ISitecoreService service)
+        {
+            _service = service;
+        }
+
+        public SearchController() : this(new SitecoreContext()) { }
+
+        #endregion
+
+        public ActionResult SearchResults()
+        {
+            return View();
+        }
+
+        public ActionResult SearchInput(SearchInput model)
+        {
+            if (!ModelState.IsValid || string.IsNullOrWhiteSpace(model?.Query)) return View(new SearchInput());
+            var searchResultsPagePath = RenderingContext.Current.Rendering.DataSource;
+            if(string.IsNullOrWhiteSpace(searchResultsPagePath))return View(new SearchInput());
+
+            var searchResultsPageItem = _service.Database.GetItem(searchResultsPagePath);
+            if (searchResultsPageItem == null) return View(new SearchInput());
+
+            var redirectUrl = $"{LinkManager.GetItemUrl(searchResultsPageItem)}?q={Server.UrlEncode(model.Query)}";
+            return Redirect(redirectUrl);
+        }
+    }
+}
