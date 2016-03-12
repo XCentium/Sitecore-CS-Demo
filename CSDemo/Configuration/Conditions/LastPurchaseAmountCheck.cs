7ab55e9bf640b306c7b3d4e5161ef54e0d5ea3be
Vasiliy Fomichev <vasya.fomichev@gmail.com>
2016-03-11 19:49:38 -0800

    Added returned visitor history check.

--- CSDemo/Configuration/Conditions/LastPurchaseAmountCheck.cs
+++ CSDemo/Configuration/Conditions/LastPurchaseAmountCheck.cs
@@ -17,43 +17,22 @@ using System.Web.Security;
 
 namespace CSDemo.Configuration.Conditions
 {
-    public class ReturnedVisitorAfterPurchase<T> :  IntegerComparisonCondition<T> where T : RuleContext
+    public class LastPurchaseAmountCheck<T> :  IntegerComparisonCondition<T> where T : RuleContext
     {
         public decimal Amount { get; set; }
 
         protected override bool Execute(T ruleContext)
         {
             if (ruleContext == null) return false;
-            
-
+           
             var automationRuleContext = ruleContext as AutomationRuleContext;
             if (automationRuleContext == null) return false;
             var contact = automationRuleContext.Contact;
-            
-            var contactFactory = new ContactFactory();
-            var userId = contactFactory.GetUserId(contact);
-            var submitRequest = new GetVisitorOrdersRequest(userId, "XCentiumCSDemo");
-            var provider = new CommerceOrderServiceProvider();
 
-            var orders = provider.GetVisitorOrders(submitRequest);
-            var commerceOrders = new List<CommerceOrder>();
-            if (orders != null)
-            {
-                var cartHelper = new CartHelper();
-                foreach (var order in orders.OrderHeaders)
-                {
-                    var orderRequest = new GetVisitorOrderRequest(order.OrderID, order.CustomerId, "XCentiumCSDemo");
-                    if (orderRequest == null) continue;
-                    var orderResult = provider.GetVisitorOrder(orderRequest);
-                    if (orderResult == null) continue;
-                    var commerceOrderHead = orderResult.Order as CommerceOrder;
-                    if (commerceOrderHead == null) continue;
+            var mostRecentOrder = Order.GetMostRecentOrder(contact);
+            if (mostRecentOrder == null) return false;
 
-                    commerceOrders.Add(commerceOrderHead);
-                }
-            }
-
-            decimal lastPurchaseAmount = 1;
+            decimal lastPurchaseAmount = mostRecentOrder.Total.Amount;
             switch (GetOperator())
             {
                 case ConditionOperator.Equal:
@@ -72,6 +51,5 @@ namespace CSDemo.Configuration.Conditions
                     return false;
             }
         }
-
     }
 }
---
+++ CSDemo/Models/Account/Order.cs
@@ -0,0 +1,45 @@
+﻿using CSDemo.Models.Checkout.Cart;
+using Sitecore.Analytics.Tracking;
+using Sitecore.Commerce.Connect.CommerceServer.Orders;
+using Sitecore.Commerce.Connect.CommerceServer.Orders.Models;
+using Sitecore.Commerce.Contacts;
+using Sitecore.Commerce.Services.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSDemo.Models.Account
+{
+    public class Order
+    {
+        public static CommerceOrder GetMostRecentOrder(Contact contact)
+        {
+            var contactFactory = new ContactFactory();
+            var userId = contactFactory.GetUserId(contact);
+            var submitRequest = new GetVisitorOrdersRequest(userId, "XCentiumCSDemo");
+            var provider = new CommerceOrderServiceProvider();
+
+            var orders = provider.GetVisitorOrders(submitRequest);
+            var commerceOrders = new List<CommerceOrder>();
+            if (orders != null)
+            {
+                var cartHelper = new CartHelper();
+                foreach (var order in orders.OrderHeaders)
+                {
+                    var orderRequest = new GetVisitorOrderRequest(order.OrderID, order.CustomerId, "XCentiumCSDemo");
+                    if (orderRequest == null) continue;
+                    var orderResult = provider.GetVisitorOrder(orderRequest);
+                    if (orderResult == null) continue;
+                    var commerceOrderHead = orderResult.Order as CommerceOrder;
+                    if (commerceOrderHead == null) continue;
+
+                    commerceOrders.Add(commerceOrderHead);
+                }
+            }
+
+            var mostRecentOrder = commerceOrders.OrderByDescending(o => o.Created).First();
+            return mostRecentOrder;
+        }
+    }
+}
