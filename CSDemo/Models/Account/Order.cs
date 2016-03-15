bfe9e40993e96f16ff654ef4c94e24e8be906af7
Vasiliy Fomichev <vasya.fomichev@gmail.com>
2016-03-14 19:23:22 -0700

    Updated code, added try/catch

--- CSDemo/Models/Account/Order.cs
+++ CSDemo/Models/Account/Order.cs
@@ -4,6 +4,7 @@ using Sitecore.Commerce.Connect.CommerceServer.Orders;
 using Sitecore.Commerce.Connect.CommerceServer.Orders.Models;
 using Sitecore.Commerce.Contacts;
 using Sitecore.Commerce.Services.Orders;
+using Sitecore.Diagnostics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,31 +16,42 @@ namespace CSDemo.Models.Account
     {
         public static CommerceOrder GetMostRecentOrder(Contact contact)
         {
-            var contactFactory = new ContactFactory();
-            var userId = contactFactory.GetUserId(contact);
-            var submitRequest = new GetVisitorOrdersRequest(userId, "XCentiumCSDemo");
-            var provider = new CommerceOrderServiceProvider();
-
-            var orders = provider.GetVisitorOrders(submitRequest);
-            var commerceOrders = new List<CommerceOrder>();
-            if (orders != null)
+            try
             {
-                var cartHelper = new CartHelper();
-                foreach (var order in orders.OrderHeaders)
+                var contactFactory = new ContactFactory();
+                var userName = contactFactory.GetUserId(contact);
+                if (string.IsNullOrWhiteSpace(userName)) return null;
+                var userId = (new AccountHelper()).GetCommerceUserID(userName);
+                if (string.IsNullOrWhiteSpace(userId)) return null;
+                var submitRequest = new GetVisitorOrdersRequest(userId, "XCentiumCSDemo");
+                var provider = new CommerceOrderServiceProvider();
+                var orders = provider.GetVisitorOrders(submitRequest);
+                var commerceOrders = new List<CommerceOrder>();
+                if (orders != null)
                 {
-                    var orderRequest = new GetVisitorOrderRequest(order.OrderID, order.CustomerId, "XCentiumCSDemo");
-                    if (orderRequest == null) continue;
-                    var orderResult = provider.GetVisitorOrder(orderRequest);
-                    if (orderResult == null) continue;
-                    var commerceOrderHead = orderResult.Order as CommerceOrder;
-                    if (commerceOrderHead == null) continue;
+                    var cartHelper = new CartHelper();
+                    foreach (var order in orders.OrderHeaders)
+                    {
+                        var orderRequest = new GetVisitorOrderRequest(order.OrderID, order.CustomerId, "XCentiumCSDemo");
+                        if (orderRequest == null) continue;
+                        var orderResult = provider.GetVisitorOrder(orderRequest);
+                        if (orderResult == null) continue;
+                        var commerceOrderHead = orderResult.Order as CommerceOrder;
+                        if (commerceOrderHead == null) continue;
 
-                    commerceOrders.Add(commerceOrderHead);
+                        commerceOrders.Add(commerceOrderHead);
+                    }
                 }
+
+                var mostRecentOrder = commerceOrders.OrderByDescending(o => o.Created).First();
+                return mostRecentOrder;
+            }
+            catch (NullReferenceException)
+            {
+                //do nothing; commerce server throws an exception for an unknown user or no orders
+                return null;
             }
 
-            var mostRecentOrder = commerceOrders.OrderByDescending(o => o.Created).First();
-            return mostRecentOrder;
         }
     }
 }
