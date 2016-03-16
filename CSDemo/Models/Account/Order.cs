7f170d1a611ac27f2f7578f34e6f0d613d5824fa
Vasiliy Fomichev <vasya.fomichev@gmail.com>
2016-03-16 10:25:35 -0700

    updated order type logic for getting orders.

--- CSDemo/Models/Account/Order.cs
+++ CSDemo/Models/Account/Order.cs
@@ -1,12 +1,19 @@
-﻿using CSDemo.Models.Checkout.Cart;
+﻿
+using CommerceServer.Core;
+using CSDemo.Models.Checkout.Cart;
 using Sitecore.Analytics.Tracking;
+using Sitecore.Commerce.Connect.CommerceServer;
 using Sitecore.Commerce.Connect.CommerceServer.Orders;
 using Sitecore.Commerce.Connect.CommerceServer.Orders.Models;
 using Sitecore.Commerce.Contacts;
 using Sitecore.Commerce.Services.Orders;
 using Sitecore.Diagnostics;
+using Sitecore.Security.Accounts;
+using Sitecore.Sites;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 
@@ -16,42 +23,67 @@ namespace CSDemo.Models.Account
     {
         public static CommerceOrder GetMostRecentOrder(Contact contact)
         {
-            try
-            {
-                var contactFactory = new ContactFactory();
-                var userName = contactFactory.GetUserId(contact);
-                if (string.IsNullOrWhiteSpace(userName)) return null;
-                var userId = (new AccountHelper()).GetCommerceUserID(userName);
-                if (string.IsNullOrWhiteSpace(userId)) return null;
-                var submitRequest = new GetVisitorOrdersRequest(userId, "XCentiumCSDemo");
-                var provider = new CommerceOrderServiceProvider();
-                var orders = provider.GetVisitorOrders(submitRequest);
-                var commerceOrders = new List<CommerceOrder>();
-                if (orders != null)
-                {
-                    var cartHelper = new CartHelper();
-                    foreach (var order in orders.OrderHeaders)
-                    {
-                        var orderRequest = new GetVisitorOrderRequest(order.OrderID, order.CustomerId, "XCentiumCSDemo");
-                        if (orderRequest == null) continue;
-                        var orderResult = provider.GetVisitorOrder(orderRequest);
-                        if (orderResult == null) continue;
-                        var commerceOrderHead = orderResult.Order as CommerceOrder;
-                        if (commerceOrderHead == null) continue;
+            var contextManager = new CommerceServerContextManager();  //using Sitecore.Commerce.Connect.CommerceServer;
+            var orderManagementContext = contextManager.OrderManagementContext;
+            var orderManager = orderManagementContext.PurchaseOrderManager;
 
-                        commerceOrders.Add(commerceOrderHead);
+            CultureInfo culture = new CultureInfo("en-US");
+            DataSet searchableProperties = orderManager.GetSearchableProperties(culture.ToString());
+            SearchClauseFactory searchClauseFactory = orderManager.GetSearchClauseFactory(searchableProperties, "PurchaseOrder"); //using CommerceServer.Core; Assembly CommerceServer.Core.CrossTier
+            SearchClause searchClause = searchClauseFactory.CreateClause(ExplicitComparisonOperator.OnOrAfter, "Created", DateTime.Now.AddMonths(-6));
+
+            SearchOptions options = new SearchOptions();
+            options.SetPaging(10, 1);
+
+            var result = orderManager.SearchPurchaseOrders(searchClause, options);
+
+
+            using (new SiteContextSwitcher(Sitecore.Configuration.Factory.GetSite("XCentiumCSDemo")))
+            {
+                try
+                {
+
+                    var contactFactory = new ContactFactory();
+                    var userName = contactFactory.GetUserId(contact);
+                    var user = Sitecore.Security.Accounts.User.FromName(userName, true);
+                    using (new UserSwitcher(user))
+                    {
+
+                        if (string.IsNullOrWhiteSpace(userName)) return null;
+                        var userId = (new AccountHelper()).GetCommerceUserID(userName);
+                        if (string.IsNullOrWhiteSpace(userId)) return null;
+                        var submitRequest = new GetVisitorOrdersRequest(userId, "XCentiumCSDemo");
+                        var provider = new CommerceOrderServiceProvider();
+                        var orders = provider.GetVisitorOrders(submitRequest);
+                        var commerceOrders = new List<CommerceOrder>();
+                        if (orders != null)
+                        {
+                            var cartHelper = new CartHelper();
+                            foreach (var order in orders.OrderHeaders)
+                            {
+                                var orderRequest = new GetVisitorOrderRequest(order.OrderID, order.CustomerId, "XCentiumCSDemo");
+                                if (orderRequest == null) continue;
+                                var orderResult = provider.GetVisitorOrder(orderRequest);
+                                if (orderResult == null) continue;
+                                var commerceOrderHead = orderResult.Order as CommerceOrder;
+                                if (commerceOrderHead == null) continue;
+
+                                commerceOrders.Add(commerceOrderHead);
+                            }
+                        }
+
+                        var mostRecentOrder = commerceOrders.OrderByDescending(o => o.Created).First();
+                        return mostRecentOrder;
                     }
                 }
+                catch (NullReferenceException ex)
+                {
+                    //do nothing; commerce server throws an exception for an unknown user or no orders
+                    return null;
+                }
 
-                var mostRecentOrder = commerceOrders.OrderByDescending(o => o.Created).First();
-                return mostRecentOrder;
-            }
-            catch (NullReferenceException)
-            {
-                //do nothing; commerce server throws an exception for an unknown user or no orders
-                return null;
-            }
 
+            }
         }
     }
 }
