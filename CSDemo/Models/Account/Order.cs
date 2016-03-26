5482311bace33007b9cbf2ca6ad70e786177a68d
Vasiliy Fomichev <vasya.fomichev@gmail.com>
2016-03-25 20:14:29 -0700

    Added order amount check.

--- CSDemo/Models/Account/Order.cs
+++ CSDemo/Models/Account/Order.cs
@@ -1,12 +1,16 @@
 ﻿
 using CommerceServer.Core;
 using CSDemo.Models.Checkout.Cart;
+using Sitecore.Analytics.Outcome;
+using Sitecore.Analytics.Outcome.Model;
 using Sitecore.Analytics.Tracking;
 using Sitecore.Commerce.Connect.CommerceServer;
 using Sitecore.Commerce.Connect.CommerceServer.Orders;
 using Sitecore.Commerce.Connect.CommerceServer.Orders.Models;
 using Sitecore.Commerce.Contacts;
 using Sitecore.Commerce.Services.Orders;
+using Sitecore.Configuration;
+using Sitecore.Data;
 using Sitecore.Diagnostics;
 using Sitecore.Security.Accounts;
 using Sitecore.Sites;
@@ -21,6 +25,19 @@ namespace CSDemo.Models.Account
 {
     public class Order
     {
+        public static decimal GetLastOrderAmount(Contact contact)
+        {
+            var manager = Factory.CreateObject("outcome/outcomeManager", true) as OutcomeManager;
+            var outcomes = manager.GetForEntity<ContactOutcome>(new ID(contact.ContactId));
+            if (outcomes == null || !outcomes.Any()) return 0;
+            var purchaseOutcomes = outcomes.OrderByDescending(o => o.DateTime).Where(o => o.Id == new ID(Constants.Commerce.PurchaseOutcomeId)).ToList();
+            if (purchaseOutcomes == null || !purchaseOutcomes.Any()) return 0;
+
+            var latestPurchaseOutcome = purchaseOutcomes.First();
+            var latestPurchaseAmount = latestPurchaseOutcome.MonetaryValue;
+            return latestPurchaseAmount;
+        }
+
         public static CommerceOrder GetMostRecentOrder(Contact contact)
         {
             var contextManager = new CommerceServerContextManager();  //using Sitecore.Commerce.Connect.CommerceServer;
