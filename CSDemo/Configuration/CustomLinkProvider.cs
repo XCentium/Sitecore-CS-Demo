b0f83c5cdd230896a462215d51e0547ac29815a5
ola <owolawi@hotmail.com>
2016-04-29 12:09:49 -0500

    Resolved Customlinkprovider

--- CSDemo/Configuration/CustomLinkProvider.cs
+++ CSDemo/Configuration/CustomLinkProvider.cs
@@ -2,6 +2,7 @@
 using Sitecore;
 using Sitecore.Data;
 using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
 using Sitecore.Links;
 
 namespace CSDemo.Configuration
@@ -10,19 +11,28 @@ namespace CSDemo.Configuration
     {
         public override string GetItemUrl(Item item, UrlOptions options)
         {
-            if (item == null) return base.GetItemUrl(item, options);
-            if (item.Template.BaseTemplates.Any(t => t.ID == new ID(Constants.Commerce.ProductBaseTemplateId)))
+            try
             {
-                var categoryUrl = GetCategoryUrl(item.Parent, options);
-                return $"{categoryUrl}/{(options.LowercaseUrls ? item.Name.ToLower() : item.Name)}";
-            }
+                if (item == null) return base.GetItemUrl(item, options);
+                if (item.Template.BaseTemplates.Any(t => t.ID == new ID(Constants.Commerce.ProductBaseTemplateId)))
+                {
+                    var categoryUrl = GetCategoryUrl(item.Parent, options);
+                    return $"{categoryUrl}/{(options.LowercaseUrls ? item.Name.ToLower() : item.Name)}";
+                }
 
-            if (item.Template.BaseTemplates.Any(t => t.ID == new ID(Constants.Commerce.CategoryBaseTemplateId)))
+                if (item.Template.BaseTemplates.Any(t => t.ID == new ID(Constants.Commerce.CategoryBaseTemplateId)))
+                {
+                    return GetCategoryUrl(item, options);
+                }
+
+                return base.GetItemUrl(item, options);
+            }
+            catch (System.Exception ex)
             {
-                return GetCategoryUrl(item, options);
-            }
+                Log.Error("Error", ex, this);
+                return string.Empty;
 
-            return base.GetItemUrl(item, options);
+            }
         }
 
         #region Private Helpers
