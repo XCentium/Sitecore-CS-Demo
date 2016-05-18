ffeb0873853115bac026703d88cc1df288177ff3
Vasiliy Fomichev <vasiliy.fomichev@xcentium.com>
2016-05-17 18:01:04 -0700

    Fixed the order for stores.

--- CSDemo/Models/Store/Store.cs
+++ CSDemo/Models/Store/Store.cs
@@ -24,20 +24,20 @@ namespace CSDemo.Models.Store
         {
             var response = string.Empty;
             Log.Info("Sorting started", origin);
-            try
-            {
-                var url = string.Format(Constants.Store.GoogleLocationMatrixApiUrl);
-                var query = $"origins={origin.Latitude},{origin.Longitude}&destinations={string.Join("|", destinations.Select(d => !string.IsNullOrWhiteSpace(d.Latitude.ToString()) && !string.IsNullOrWhiteSpace(d.Longitude.ToString()) ? $"{d.Latitude},{d.Longitude}" : $"{d.Street} {d.City} {d.State} {d.Zip}"))}";
-                var syncClient = new WebClient();
-                Log.Info($"{url}?{query}", syncClient);
-                response = syncClient.DownloadString($"{url}?{query}");
-                Log.Info("Google response: " + response, syncClient);
-            }
-            catch (Exception ex)
-            {
-                Log.Error("Unable to get coors from Google: " + ex.Message, ex);
-            }
-
+            //try
+            //{
+            //    var url = string.Format(Constants.Store.GoogleLocationMatrixApiUrl);
+            //    var query = $"origins={origin.Latitude},{origin.Longitude}&destinations={string.Join("|", destinations.Select(d=> !string.IsNullOrWhiteSpace(d.Latitude.ToString()) && !string.IsNullOrWhiteSpace(d.Longitude.ToString()) ? $"{d.Latitude},{d.Longitude}": $"{d.Street} {d.City} {d.State} {d.Zip}"))}";
+            //    var syncClient = new WebClient();
+            //    Log.Info($"{url}?{query}", syncClient);
+            //    response = syncClient.DownloadString($"{url}?{query}");
+            //    Log.Info("Google response: "+response, syncClient);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Log.Error("Unable to get coors from Google: "+ex.Message, ex);
+            //}
+            response = "{\"destination_addresses\":[\"6762 N Glenwood St, Boise, ID 83714, USA\",\"8900 Viscount Blvd, El Paso, TX 79925, USA\",\"4275 E Charleston Blvd, Las Vegas, NV 89104, USA\",\"14014 NW Passage, Marina Del Rey, CA 90292, USA\"],\"origin_addresses\":[\"24514 Town Center Dr, Santa Clarita, CA 91355, USA\"],\"rows\":[{\"elements\":[{\"distance\":{\"text\":\"1,320 km\",\"value\":1319829},\"duration\":{\"text\":\"12 hours 56 mins\",\"value\":46550},\"status\":\"OK\"},{\"distance\":{\"text\":\"1,358 km\",\"value\":1357536},\"duration\":{\"text\":\"12 hours 8 mins\",\"value\":43679},\"status\":\"OK\"},{\"distance\":{\"text\":\"450 km\",\"value\":449718},\"duration\":{\"text\":\"4 hours 23 mins\",\"value\":15761},\"status\":\"OK\"},{\"distance\":{\"text\":\"64.4 km\",\"value\":64446},\"duration\":{\"text\":\"53 mins\",\"value\":3156},\"status\":\"OK\"}]}],\"status\":\"OK\"}";
             if (string.IsNullOrEmpty(response)) return null;
             
             var result = JsonConvert.DeserializeObject<dynamic>(response);
@@ -67,7 +67,7 @@ namespace CSDemo.Models.Store
                     }
                 }
             }
-            return distances.OrderByDescending(i=>i.Value).Select(i=>i.Key);
+            return distances.OrderBy(i=>i.Value).Select(i=>i.Key);
         }
 
         #endregion
