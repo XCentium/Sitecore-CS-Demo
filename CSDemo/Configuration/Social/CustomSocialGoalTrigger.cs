10eaef864cd28a9f962c450fa907cef69bd3261f
Sean.Fox <sean.fox@xcentium.com>
2016-05-24 11:28:19 -0700

    [CSDEMO#157] added custom automation action (which triggers a goal with associated message)

---
+++ CSDemo/Configuration/Social/CustomSocialGoalTrigger.cs
@@ -0,0 +1,25 @@
+﻿using Castle.Core.Internal;
+using Sitecore.Analytics.Automation;
+using Sitecore.Diagnostics;
+
+namespace CSDemo.Configuration.Social
+{
+    public class CustomSocialGoalTrigger : IAutomationAction
+    {
+        public AutomationActionResult Execute(AutomationActionContext context)
+        {
+            Assert.ArgumentNotNull(context, "context");
+            if (!Sitecore.Analytics.Tracker.IsActive || Sitecore.Analytics.Tracker.Current.CurrentPage == null)
+                return AutomationActionResult.Continue;
+            Sitecore.Data.Items.Item socialMessageGoalItem = Sitecore.Context.Database.GetItem(Constants.Cart.AbandonedCartSocialGoalItemId);
+            if (socialMessageGoalItem != null)
+            {
+                Sitecore.Analytics.Data.Items.PageEventItem registerthegoal = new Sitecore.Analytics.Data.Items.PageEventItem(socialMessageGoalItem);
+                Sitecore.Analytics.Model.PageEventData eventData = Sitecore.Analytics.Tracker.Current.CurrentPage.Register(registerthegoal);
+                eventData.Data = socialMessageGoalItem["Description"];
+                Sitecore.Analytics.Tracker.Current.Interaction.AcceptModifications();
+            }
+            return AutomationActionResult.Continue;
+        }
+    }
+}
--- CSDemo/Constants.cs
+++ CSDemo/Constants.cs
@@ -5,80 +5,61 @@
         public const string DefaultSiteName = "Website";
         public const string DeliveryDatabase = "web";
         public const string WebIndex = "sitecore_web_index";
-
         public struct QueryStrings
         {
             /// <summary>
             /// Used for paging
             /// </summary>
             public const string Paging = "pn";
-
             /// <summary>
             /// Used for the sorting field
             /// </summary>
             public const string Sort = "s";
-
             /// <summary>
             /// Used for the sorting field direction
             /// </summary>
             public const string SortDirection = "sd";
-
             /// <summary>
             /// Used for facets
             /// </summary>
             public const string Facets = "f";
-
             /// <summary>
             /// Used for separating facets
             /// </summary>
             public const char FacetsSeparator = '|';
-
             public const string FacetOptionSeparator = "||";
-
             public const char FacetOptionDefinitionSeparator = '=';
-
             /// <summary>
             /// Used for the search keyword
             /// </summary>
             public const string SearchQuery = "q";
-
             /// <summary>
             /// Used for page size
             /// </summary>
             public const string PageSize = "ps";
         }
-
         public struct Commerce
         {
-
             public const string DefaultCurrencyCode = "USD";
-
             public static string[] DefaultPriceTypeIds = new string[2] { "List", "Adjusted" };
-
             public const string Departments = "Departments";
             public const string CategoryDepartments = "categories/departments";
-
             public const string ProductBaseTemplateId = "{225F8638-2611-4841-9B89-19A5440A1DA1}";
             public const string CategoryBaseTemplateId = "{4C4FD207-A9F7-443D-B32A-50AA33523661}";
             public const string PurchaseOutcomeId = "{9016E456-95CB-42E9-AD58-997D6D77AE83}";
             public const string ProductTagsFieldId = "{CB2364DB-F3ED-46D2-AAC4-CFE3A280E7DD}";
             public const string ProductTagNameFieldId = "{B5620C00-330B-479E-9B54-57857040E1E0}";
-
             public const string DefaultSocialDomainForCommerce = "CommerceUsers";
             public const string CommerceUserDomain = "CommerceUser";
-
             public const string CommerceCustomerId = "scommerce_customer_id";
             public const string CommerceUserId = "user_id";
             public const string CommerceUserCatalogSetId = "user_catalog_set";
-
             public const string CommerceUserLoggedIn = "CommerceUserLoggedIn";
             public const string UserCatalogOptions = "UserCatalogOptions";
             public const string UserSelectedCatalogId = "UserSelectedCatalogId";
             public const string ShowCoupon = "ShowCoupon";
             public const string CouponMessage = "CouponMessage";
-
         }
-
         public struct Marketing
         {
             public const string AddToCartGoalId = "{027F31D6-E67E-49C8-B7B2-C589BD62D7B0}";
@@ -86,7 +67,6 @@
             public const string SubmitOrderGoalId = "{00F026E7-D067-4546-8C6F-2E67E591494A}";
             public const string StockNotificationEngagementPlanId = "{B8249271-2721-4A1F-BBFA-9F52D5A9B5F2}";
         }
-
         public struct Account
         {
             public const string CustomerPhotoPath = "/sitecore/media library/CSDemo/Customers/";
@@ -102,16 +82,11 @@
             public const string SigninErrorMsg2 = "?msg=Incorrect Username or Password";
             public const string CatalogSetField = "IncludedCatalogs";
             public const string Error = "Error";
-
-
         }
-
-
         public struct Sitecore
         {
             public const string AnalyticsIndexName = "sitecore_analytics_index";
         }
-
         public struct Common
         {
             public const string Dash = "-";
@@ -123,21 +98,16 @@
             public const char PipeSeparator = '|';
             public const string PipeStringSeparator = "|";
             public const string True = "true";
-
         }
-
         public struct Page
         {
             public const string MetaDescription = "Meta Description";
             public const string PageTitle = "Page Title";
-
         }
-
         public struct Store
         {
             public const string GoogleLocationMatrixApiUrl = "https://maps.googleapis.com/maps/api/distancematrix/json";
         }
-
         public struct Products
         {
             public const string IdSeparator = "|";
@@ -178,7 +148,6 @@
             public const string NotificationError = "?msg=error";
             public const string FeaturedProducts = "FeaturedProducts";
         }
-
         public struct Cart
         {
             public const string AnonUserActionDenied = "Action DENIED! Only Visitors or Signed in Customers Allowed";
@@ -191,9 +160,8 @@
             public const string CartInvalidInCache = "CartCacheHelper::InvalidateCartCache - Cart for customer id {0} is not in the cache!";
             public const string CartAlreadyInCache = "CartCacheHelper::AddCartToCache - Cart for customer id {0} is already in the cache!";
             public const string ErrorInBasket = "Error in Basket";
+            public const string AbandonedCartSocialGoalItemId = "{D2D3D9F2-4DFE-452F-9BEF-F1E79EDD0A11}";
         }
-
-
         public struct Blog
         {
             public const string Category = "category";
@@ -203,6 +171,5 @@
             public const string AuthorsPath = "/sitecore/content/Data/Blogs/Authors";
             public const string CategoriesPath = "/sitecore/content/Data/Blogs/Categories";
         }
-
     }
 }
