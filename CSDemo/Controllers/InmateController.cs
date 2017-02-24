b1efffbc460993d075a4cb9ec8472032716ba6be
Vasiliy Fomichev <vasya.fomichev@gmail.com>
2017-02-24 10:24:59 -0800

    Added inmate selector.

---
+++ CSDemo/Controllers/InmateController.cs
@@ -0,0 +1,41 @@
+﻿#region
+
+using Sitecore;
+using Sitecore.Mvc.Controllers;
+using System.Web.Mvc;
+using CSDemo.Models.Checkout.Cart;
+using CSDemo.Models.Inmate;
+
+#endregion
+
+namespace CSDemo.Controllers
+{
+    public class InmateController : SitecoreController
+    {
+        [HttpGet]
+        public ActionResult InmateSelector()
+        {
+            return View(new InmateSelector());
+        }
+
+
+        [HttpPost]
+        public ActionResult InmateSelector(InmateSelector model)
+        {
+            if (!ModelState.IsValid) return Redirect("/");
+
+            switch (model.SelectedPrisonId)
+            {
+                case "20021":
+                    return Redirect(string.Format("http://keefeca.xcentium.net?pid={0}&iid={1}",
+                        model.SelectedPrisonId, model.InmateId));
+                case "20114":
+                    return Redirect(string.Format("http://keefeoh.xcentium.net?pid={0}&iid={1}",
+                        model.SelectedPrisonId,  model.InmateId));
+                default:
+                    return Redirect(string.Format("http://keefeca.xcentium.net?pid={0}&iid={1}", 
+                        model.SelectedPrisonId, model.InmateId));
+            }
+        }
+    }
+}
---
+++ CSDemo/Models/Inmate/InmateSelector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSDemo.Models.Inmate
+{
+    public class InmateSelector
+    {
+        public string InmateId { get; set; }
+        public string SelectedPrisonId { get; set; }
+    }
+}
