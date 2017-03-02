2330ece9cf5499a960bc8aa671922bf5c83d1dd6
Vasiliy Fomichev <vasya.fomichev@gmail.com>
2017-03-02 15:34:23 -0800

    Added inmate selector and new URL-based rule

---
+++ CSDemo/Configuration/Conditions/UrlComparison.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Rules;
+using Sitecore.Rules.Conditions;
+using System.Web;
+
+namespace CSDemo.Configuration.Conditions
+{
+    public class CheckQuerystringCondition<T> : StringOperatorCondition<T> where T : RuleContext
+    {
+        public string Value { get; set; }
+
+        protected override bool Execute(T ruleContext)
+        {
+            Assert.ArgumentNotNull(ruleContext, "ruleContext");
+            var url = HttpContext.Current.Request.Url.ToString();
+            return Compare(url, Value);
+        }
+    }
+}
---
+++ CSDemo/Models/InmateSelector.cs
@@ -0,0 +1,8 @@
+﻿namespace CSDemo.Models.Inmate
+{
+    public class InmateSelector
+    {
+        public string InmateId { get; set; }
+        public string SelectedPrisonId { get; set; }
+    }
+}
