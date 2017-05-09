029b1569b9b528c696cc719b89237c29f81e26a5
John <John@XC-JOHN>
2017-05-09 13:36:00 -0700

    8.2.1 - FSS Demo serialization and changes

---
+++ CSDemo/Tools/ItemTester.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ItemTester.aspx.cs" Inherits="CSDemo.Tools.ItemTester" %>
+<%@ import namespace="Sitecore" %>
+<%@ Import Namespace="Sitecore.Data" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Item Tester</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ CSDemo/Tools/ItemTester.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CSDemo.Helpers;
+using CSDemo.Models.Product;
+using Sitecore.Commerce.Connect.CommerceServer.Controls;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Shell.Applications.ContentEditor;
+
+namespace CSDemo.Tools
+{
+    public partial class ItemTester : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var itemId = new ID("{833102C4-765F-4FA5-8DBD-A7F785D41659}"); //boots category
+            var item = Sitecore.Context.Database.GetItem(itemId);
+
+            string name;
+            if (item != null)
+            {
+                var cat = GlassHelper.Cast<Category>(item);
+                var x = cat.ChildProducts;
+
+                ChildProductsListControl y = new ChildProductsListControl();
+            }
+        }
+    }
+}
