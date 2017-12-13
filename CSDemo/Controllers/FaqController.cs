3f8419d687bc052c403fad83cad29c1481b634a7
Josh Morales <josh.morales@xcentium.com>
2017-12-13 10:37:18 -0800

    faq controller

---
+++ CSDemo/Controllers/FaqController.cs
@@ -0,0 +1,30 @@
+﻿using CSDemo.Models.Faq;
+using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSDemo.Controllers
+{
+	public class FaqController : Controller
+	{
+		const string FAQ_LOCATION = "/sitecore/content/Global Configuration/FAQs";
+		public ActionResult List()
+		{
+			var model = new FaqViewModel();
+
+			var faqItem = Sitecore.Context.Database.GetItem(FAQ_LOCATION);
+			foreach (Item faq in faqItem.GetChildren())
+			{
+				var faqModel = new FaqItem();
+				faqModel.Question = faq["Question"];
+				faqModel.Answer = faq["Answer"];
+				model.Faqs.Add(faqModel);
+			}
+
+			return View(model);
+		}
+	}
+}
---
+++ CSDemo/Models/Faq/FaqViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSDemo.Models.Faq
+{
+	public class FaqItem
+	{
+		public string Question { get; set; }
+		public string Answer { get; set; }
+	}
+
+	public class FaqViewModel
+	{
+		public List<FaqItem> Faqs { get; set; } = new List<FaqItem>();
+	}
+}
