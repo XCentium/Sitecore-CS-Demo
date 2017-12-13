d226dffe7c3905cb6ad96be7189c649bdd832aba
Josh Morales <josh.morales@xcentium.com>
2017-12-13 10:36:36 -0800

    program and global messaging

---
+++ CSDemo/Controllers/ProgramInfoController.cs
@@ -0,0 +1,29 @@
+﻿using CSDemo.Models.ProgramInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSDemo.Controllers
+{
+	public class ProgramInfoController : Controller
+	{
+		public ActionResult Message()
+		{
+			var item = Sitecore.Context.Item;
+			Sitecore.Data.Database contextDB = Sitecore.Configuration.Factory.GetDatabase(item.Database.Name);
+			var siteName = Sitecore.Context.Site.ContentStartPath;
+			var siteNode = contextDB.GetItem(siteName);
+			var programId = siteNode["Program"];
+			var program = contextDB.GetItem(programId);
+
+			var model = new MessageViewModel();
+			model.Message = program["Program Message"];
+			Sitecore.Data.Fields.ImageField linkField = program.Fields["Program Image"];
+			
+			model.ImageSrc = Sitecore.Resources.Media.MediaManager.GetMediaUrl(linkField.MediaItem);
+			return View(model);
+		}
+	}
+}
---
+++ CSDemo/Models/ProgramInfo/MessageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSDemo.Models.ProgramInfo
+{
+	public class MessageViewModel
+	{
+		public string Message { get; set; }
+		public string ImageSrc { get; set; }
+	}
+}
