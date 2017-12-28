9bf7b7877728b0ec08e523067a5d77b94b0e1848
Josh Morales <josh.morales@xcentium.com>
2017-12-28 11:13:16 -0800

    Add support for variable FAQ

--- CSDemo/Controllers/FaqController.cs
+++ CSDemo/Controllers/FaqController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,6 +13,8 @@ namespace CSDemo.Controllers
 	public class FaqController : Controller
 	{
 		const string FAQ_LOCATION = "/sitecore/content/Global Configuration/FAQs";
+		const string VARIABLE_FAQ_LOCATION = "/sitecore/content/Global Configuration/Variable FAQs";
+
 		public ActionResult List()
 		{
 			Item item = Sitecore.Context.Item;
@@ -21,6 +24,7 @@ namespace CSDemo.Controllers
 			var siteNode = contextDB.GetItem(siteName);
 
 			var program = siteNode["Program"];
+			var programItem = contextDB.GetItem(program);
 			Debug.WriteLine("Current Program: " + program);
 
 			var model = new FaqViewModel();
@@ -43,6 +47,64 @@ namespace CSDemo.Controllers
 					//faqModel.Question = "SKIPPED: " + faq["Question"];
 					//faqModel.Answer = faq["Answer"];
 					//model.Faqs.Add(faqModel);
+				}
+			}
+
+			var varFaqItems = Sitecore.Context.Database.GetItem(VARIABLE_FAQ_LOCATION);
+			foreach (Item faq in varFaqItems.GetChildren())
+			{
+				var faqModel = new FaqItem();
+				faqModel.Question = faq["Question"];
+
+				var answers = faq["Answers"];
+				var values = HttpUtility.ParseQueryString(answers);
+
+				var sb = new StringBuilder();
+
+				foreach (string key in values.Keys)
+				{
+					var keys = key.Split("_".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+					if (keys.Length == 1)
+					{
+						var programValue = programItem[key];
+						if (!string.IsNullOrEmpty(programValue))
+						{
+							var tempAnswer = values[key];
+							tempAnswer = tempAnswer.Replace($"[{key}]", programValue);
+							sb.Append(tempAnswer);
+						}
+					}
+					else
+					{
+
+						string tempAnswer = values[key];
+
+						foreach (var fieldName in keys)
+						{
+							var programValue = programItem[fieldName];
+							if (string.IsNullOrEmpty(programValue))
+							{
+								tempAnswer = string.Empty;
+								break;
+							}
+							else
+							{
+
+								tempAnswer = tempAnswer.Replace($"[{fieldName}]", programValue);
+							}
+
+						}
+
+
+						sb.Append(tempAnswer);
+					}
+				}
+
+				var answerText = sb.ToString();
+				if (!string.IsNullOrEmpty(answerText))
+				{
+					faqModel.Answer = answerText;
+					model.Faqs.Add(faqModel);
 				}
 			}
 
